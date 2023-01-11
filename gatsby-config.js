445288ee5082bc2d84cb336dcd055e1cca5239ce
Casey K <tehcnicolour@gmail.com>
2023-01-11 11:25:35 -0800

    tagmanager

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,7 @@ module.exports = {
         // on every Gatsby route change.
         //
         // Defaults to gatsby-route-change
-        routeChangeEventName: 'YOUR_ROUTE_CHANGE_EVENT_NAME',
+        routeChangeEventName: 'gatsby-route-change',
       },
     },
     {
